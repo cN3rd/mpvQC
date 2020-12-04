0d601af065177a8de3c122bd159f07dbdcd57dd6
Elias Müller <elias.mueller@mailbox.org>
2020-12-04 22:45:37 +0100

    :white_check_mark: Add missing tests

--- test/gui/dialogs/test_app_dialogs.py
+++ test/gui/dialogs/test_app_dialogs.py
@@ -21,6 +21,22 @@ class TestAppDialogs(unittest.TestCase):
         AppDialogs.import_documents(parent=None)
         self.assertEqual(Path('/new'), mock_settings.return_value.import_last_dir_document)
 
+    @patch('mpvqc.gui.dialogs.app_dialogs.get_settings', return_value=Mocker('import_last_dir_document', Path('/old')))
+    @patch('mpvqc.gui.dialogs.app_dialogs.OpenDocumentsDialog')
+    def test_import_document_returns_documents_on_success(self, dialog: Mock, *_):
+        documents_user = tuple([Path('/new/imported.txt'), Path('/new/imported2.txt')])
+        dialog.return_value.get_documents.return_value = documents_user
+        documents_returned = AppDialogs.import_documents(parent=None)
+        self.assertEqual(documents_user, documents_returned)
+
+    @patch('mpvqc.gui.dialogs.app_dialogs.get_settings', return_value=Mocker('import_last_dir_document', Path('/old')))
+    @patch('mpvqc.gui.dialogs.app_dialogs.OpenDocumentsDialog')
+    def test_import_document_returns_documents_on_cancel(self, dialog: Mock, *_):
+        documents_user = tuple([])
+        dialog.return_value.get_documents.return_value = documents_user
+        documents_returned = AppDialogs.import_documents(parent=None)
+        self.assertEqual(documents_user, documents_returned)
+
     @patch('mpvqc.gui.dialogs.app_dialogs.get_settings', return_value=Mocker('import_last_dir_video', Path('/old')))
     @patch('mpvqc.gui.dialogs.app_dialogs.OpenVideoDialog')
     def test_import_video_latest_directory_passed_in(self, mock_dialog, *_):
@@ -34,6 +50,22 @@ class TestAppDialogs(unittest.TestCase):
         AppDialogs.import_video(parent=None)
         self.assertEqual(Path('/new'), mock_settings.return_value.import_last_dir_video)
 
+    @patch('mpvqc.gui.dialogs.app_dialogs.get_settings', return_value=Mocker('import_last_dir_video', Path('/old')))
+    @patch('mpvqc.gui.dialogs.app_dialogs.OpenVideoDialog')
+    def test_import_video_returns_video_on_success(self, dialog: Mock, *_):
+        video_user = Path('/new/imported.mp4')
+        dialog.return_value.get_video.return_value = video_user
+        video_returned = AppDialogs.import_video(parent=None)
+        self.assertEqual(video_user, video_returned)
+
+    @patch('mpvqc.gui.dialogs.app_dialogs.get_settings', return_value=Mocker('import_last_dir_video', Path('/old')))
+    @patch('mpvqc.gui.dialogs.app_dialogs.OpenVideoDialog')
+    def test_import_video_returns_video_on_cancel(self, dialog: Mock, *_):
+        video_user = None
+        dialog.return_value.get_video.return_value = video_user
+        video_returned = AppDialogs.import_video(parent=None)
+        self.assertEqual(video_user, video_returned)
+
     @patch('mpvqc.gui.dialogs.app_dialogs.get_settings', return_value=Mocker('import_last_dir_subtitles', Path('/old')))
     @patch('mpvqc.gui.dialogs.app_dialogs.OpenSubtitlesDialog')
     def test_import_subtitles_latest_directory_passed_in(self, mock_dialog, *_):
@@ -46,3 +78,19 @@ class TestAppDialogs(unittest.TestCase):
         dialog.return_value.get_subtitles.return_value = [Path('/new/imported.ass')]
         AppDialogs.import_subtitles(parent=None)
         self.assertEqual(Path('/new'), mock_settings.return_value.import_last_dir_subtitles)
+
+    @patch('mpvqc.gui.dialogs.app_dialogs.get_settings', return_value=Mocker('import_last_dir_subtitles', Path('/old')))
+    @patch('mpvqc.gui.dialogs.app_dialogs.OpenSubtitlesDialog')
+    def test_import_subtitles_returns_subtitles_on_success(self, dialog: Mock, *_):
+        subtitles_user = tuple([Path('/new/imported.ass'), Path('/new/imported2.ass')])
+        dialog.return_value.get_subtitles.return_value = subtitles_user
+        subtitles_returned = AppDialogs.import_subtitles(parent=None)
+        self.assertEqual(subtitles_user, subtitles_returned)
+
+    @patch('mpvqc.gui.dialogs.app_dialogs.get_settings', return_value=Mocker('import_last_dir_subtitles', Path('/old')))
+    @patch('mpvqc.gui.dialogs.app_dialogs.OpenSubtitlesDialog')
+    def test_import_subtitles_returns_subtitles_on_cancel(self, dialog: Mock, *_):
+        subtitles_user = tuple([])
+        dialog.return_value.get_subtitles.return_value = subtitles_user
+        subtitles_returned = AppDialogs.import_subtitles(parent=None)
+        self.assertEqual(subtitles_user, subtitles_returned)
