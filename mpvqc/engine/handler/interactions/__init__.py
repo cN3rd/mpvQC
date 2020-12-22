d526c5731b2114b937b46403735c2bc54d4b7ef2
Elias Müller <elias.mueller@mailbox.org>
2020-12-22 11:06:47 +0100

    :sparkles: Add lowest level of handler layer impls

---
+++ mpvqc/engine/handler/interactions/__init__.py
@@ -0,0 +1,28 @@
+#  mpvQC
+#
+#  Copyright (C) 2020 mpvQC developers
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+from mpvqc.engine.handler.interactions.dialog_document_import import DocumentImportDialog
+from mpvqc.engine.handler.interactions.dialog_save import SaveDialog
+from mpvqc.engine.handler.interactions.dialog_subtitle_import import SubtitleImportDialog
+from mpvqc.engine.handler.interactions.dialog_video_import import VideoImportDialog
+from mpvqc.engine.handler.interactions.message_box_existing_comments import ExistingCommentsMessageBox
+from mpvqc.engine.handler.interactions.message_box_incompatible_documents import IncompatibleDocumentsMessageBox
+from mpvqc.engine.handler.interactions.message_box_new_document import NewDocumentMessageBox
+from mpvqc.engine.handler.interactions.message_box_quit_unsaved import QuitUnsavedMessageBox
+from mpvqc.engine.handler.interactions.message_box_save_error import SaveErrorMessageBox
+from mpvqc.engine.handler.interactions.message_box_video_found import VideoFoundMessageBox
